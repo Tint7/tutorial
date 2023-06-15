594ce09aae08d773700915a6ae1718ec8647c6cf
Tint Lwin Oo <135571741+Tint7@users.noreply.github.com>
2023-06-15 17:05:57 +0630

    Car CRUD Review Fixed

--- cruddb/crudcar.dart
+++ cruddb/crudcar.dart
@@ -1,150 +1,148 @@
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'dart:io';
+import 'models/config/entity_manager.dart';
+import 'models/car.dart';
+import 'services/car_service.dart';
 
-main() async {
-  // Init ffi loader if needed.
-  sqfliteFfiInit();
+/// The current project path
+final projectPath = Directory.current.path;
 
-  var databaseFactory = databaseFactoryFfi;
-  var db = await databaseFactory.openDatabase(
-      r"C:\Users\Tint Lwin Oo\OneDrive\Desktop\SQLiteDB\crudcar.db");
-  await db.execute('''
-  CREATE TABLE IF NOT EXISTS Car (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        model TEXT
-  )
-  ''');
+/// the entiry manager
+final entityManager = EntityManager();
 
-  getOption(db);
+/// the user service
+final carService = CarService();
+
+/// main function
+Future<void> main() async {
+  await entityManager.initDatabase([Car.empty()]);
+  await showOption();
 }
 
-void getOption(Database db) async {
-  var next = true;
-  do {
-    var option = [
-      "list     - to display the list of cars",
-      "create   - to create a new car",
-      "update   - to update a car",
-      "delete   - to delete a car"
-    ];
-    for (var i = 0; i < option.length; i++) {
-      int no = i + 1;
-      print("$no. " + option[i]);
+/// Command
+///
+/// The asynchronous function to run the command
+Future<void> showOption() async {
+  print('Car CRUD operation');
+  print('------------------');
+  print('There are 4 options to control:');
+  print('1. list - display the list of users.');
+  print('2. create - create a new user.');
+  print('3. update - update an existing user.');
+  print('4. delete - delete an existing user.');
+
+  while (true) {
+    print('Enter the option: ');
+    final option = stdin.readLineSync();
+    if (option == 'exit') {
+      print('Leaving the command.');
+      break;
     }
-    stdout.write("Enter a command above you want: ");
-    String getoption = stdin.readLineSync()!.toLowerCase();
+    await executeCar(option);
+  }
+}
 
-    switch (getoption) {
-      case "list":
-        Future<void> listCars() async {
-          // Query the 'Car' table
-          List<Map<String, dynamic>> result = await db.query('Car');
-          if (result.isEmpty) {
-            print("No cars found.");
-          } else {
-            for (var element in result) {
-              print("ID: ${element['id']}");
-              print("NAME: ${element['name']}");
-              print("MODEL: ${element['model']}");
-              print("\n");
-            }
-          }
-        }
+// Execute
+///
+/// To execute the command options.
+///
+/// Parameters
+///  - [String] option
+Future<void> executeCar(String? option) async {
+  switch (option) {
+    case 'list':
+      final cars = await carService.getAllCars();
+      if (cars.isEmpty) {
+        print('First create the car!');
+      } else {
+        print(cars);
+      }
+      break;
+    case 'create':
+      await createCar();
+      break;
+    case 'update':
+      await updateCar();
+      break;
+    case 'delete':
+      await deleteCar();
+      break;
+    default:
+      print('Invalid option');
+      break;
+  }
+}
 
-        await listCars();
-        break;
+/// CreateCar
+///
+/// The asynchronous function to create user
+Future<void> createCar() async {
+  print('Creating a new Car');
+  print('-------------------');
 
-      case "create":
-        createCar() async {
-          stdout.write("Enter a new car brand name: ");
-          String name = stdin.readLineSync()!.toUpperCase();
-          stdout.write("Enter a new car model: ");
-          String model = stdin.readLineSync()!.toUpperCase();
+  final car = Car.empty();
 
-          // Insert data into the 'Car' table
-          await db
-              .insert('Car', <String, String>{'name': name, 'model': model});
-          print("You created a new car successfully!");
-          print("\n");
-        }
-        await createCar();
-        break;
+  print('Enter the name of the new car: ');
+  car.name = stdin.readLineSync();
 
-      case "update":
-        updateCar() async {
-          stdout.write("Enter ID of the car to update: ");
-          String sid = stdin.readLineSync()!;
-          int id = int.parse(sid);
+  print('Enter the model of the new car: ');
+  final modelInput = stdin.readLineSync();
+  car.model = modelInput ?? '';
 
-          // Check if car exists in the database
-          List<Map<String, dynamic>> result = await db.query(
-            'Car',
-            where: 'id = ?',
-            whereArgs: [id],
-          );
+  await carService.createCar(car);
 
-          if (result.isEmpty) {
-            print("Car with ID $id does not exist.");
-          } else {
-            stdout.write("Enter new car brand name: ");
-            String name = stdin.readLineSync()!.toUpperCase();
-            stdout.write("Enter new car model: ");
-            String model = stdin.readLineSync()!.toUpperCase();
+  print('Successfully created the new Car.');
+}
 
-            // Update the car in the database
-            await db.update(
-              'Car',
-              {'name': name, 'model': model},
-              where: 'id = ?',
-              whereArgs: [id],
-            );
+/// updateUser
+///
+/// The asynchronous function to update user
+Future<void> updateCar() async {
+  print('Updating a Car');
+  print('----------------');
 
-            print("Car with ID $id has been updated!");
-            print("\n");
-          }
-        }
+  print('Enter the car ID: ');
+  final idInput = stdin.readLineSync();
+  final id = int.tryParse(idInput ?? '') ?? 0;
 
-        await updateCar();
-        break;
+  final car = await carService.getCarById(id);
+  if (car == null) {
+    print('Car not found');
+    return;
+  }
 
-      case "delete":
-        deleteCar() async {
-          stdout.write("Enter ID of the car to delete: ");
-          String sid = stdin.readLineSync()!;
-          int id = int.parse(sid);
+  print('Enter the car name: ');
+  final nameInput = stdin.readLineSync();
+  if (nameInput?.isNotEmpty == true) {
+    car.name = nameInput!;
+  }
 
-          // Check if car exists in the database
-          List<Map<String, dynamic>> result = await db.query(
-            'Car',
-            where: 'id = ?',
-            whereArgs: [id],
-          );
+  print('Enter the car model: ');
+  final modelInput = stdin.readLineSync();
+  if (modelInput?.isNotEmpty == true) {
+    car.model = modelInput;
+  }
 
-          if (result.isEmpty) {
-            print("Car with ID $id does not exist.");
-          } else {
-            // Delete the car from the database
-            await db.delete(
-              'Car',
-              where: 'id = ?',
-              whereArgs: [id],
-            );
+  await carService.updateCar(car);
+  print('Updated the car!');
+}
 
-            print("Car with ID $id has been deleted!");
-            print("\n");
-          }
-        }
+/// DeleteUser
+///
+/// The asynchronous function to deleate user
+Future<void> deleteCar() async {
+  print('Deleting a car');
+  print('----------------');
 
-        await deleteCar();
-        break;
+  print('Enter the car ID: ');
+  final idInput = stdin.readLineSync();
+  final id = int.tryParse(idInput ?? '') ?? 0;
 
-      default:
-        {
-          print("Invalid choice!");
-          next = false;
-        }
-        break;
-    }
-  } while (next == true);
+  final car = await carService.getCarById(id);
+  if (car == null) {
+    print('Car not found');
+    return;
+  }
+
+  await carService.deleteCar(id);
+  print('Deleted the car!');
 }
